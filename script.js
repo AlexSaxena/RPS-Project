8339149b395868a3271b4d49ead624b948bdf5f3
AlexSaxena <saxenaalex@gmail.com>
2022-01-06 17:27:50 +0100

    Adds function for game logic - single round

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 console.log("Greetings, General Kenobi");
 
-function computerPlay() {
+// Function for Computer Choice
+function computerChoice() {
   let random = Math.floor(Math.random() * 3) + 1;
   if (random == 1) {
     return "rock";
@@ -8,5 +9,38 @@ function computerPlay() {
     return "paper";
   } else {
     return "scissors";
+  }
+}
+
+// Function for playing a single round of RPS
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == computerSelection) {
+    console.log(
+      `It is a tie between! ${playerSelection} and ${computerSelection}`
+    );
+  } else if (playerSelection == "rock" && computerSelection == "scissors") {
+    console.log(
+      `You won using ${playerSelection} against ${computerSelection}`
+    );
+  } else if (playerSelection == "rock" && computerSelection == "paper") {
+    console.log(
+      `You lost using ${playerSelection} against ${computerSelection}`
+    );
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    console.log(
+      `You won using ${playerSelection} against ${computerSelection}`
+    );
+  } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    console.log(
+      `You lost using ${playerSelection} against ${computerSelection}`
+    );
+  } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    console.log(
+      `You won using ${playerSelection} against ${computerSelection}`
+    );
+  } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    console.log(
+      `You lost using ${playerSelection} against ${computerSelection}`
+    );
   }
 }
