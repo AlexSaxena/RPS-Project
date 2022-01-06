b58bd294c92a4718bd890c3834d8440d2c378d99
AlexSaxena <saxenaalex@gmail.com>
2022-01-06 16:49:15 +0100

    Adds computer random choice function

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Greetings, General Kenobi");
+
+function computerPlay() {
+  let random = Math.floor(Math.random() * 3) + 1;
+  if (random == 1) {
+    return "rock";
+  } else if (random == 2) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
