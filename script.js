4f758bae936f76f209bbe99422b60d735a91d8e8
AlexSaxena <saxenaalex@gmail.com>
2022-01-20 20:15:44 +0100

    Adds Result div in html, Adds js round result output to div

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let computerScore = 0;
 let rockBtn = document.querySelector("#rock-button");
 let paperBtn = document.querySelector("#paper-button");
 let scissorsBtn = document.querySelector("#scissors-button");
+let resultBox = document.querySelector("#result-box");
 
 rockBtn.addEventListener("click", playRound);
 paperBtn.addEventListener("click", playRound);
@@ -26,31 +27,26 @@ function computerChoice() {
 function playRound(player) {
   let computerSelection = computerChoice();
   let playerChoice = player.target.innerText.toLowerCase();
-  console.log("-------------------------------------------------");
 
   if (playerChoice == computerSelection) {
-    console.log(
-      `It is a tie between! ${playerChoice} and ${computerSelection}`
-    );
+    resultBox.textContent = `It is a tie between! ${playerChoice} and ${computerSelection}`;
   } else if (playerChoice == "rock" && computerSelection == "scissors") {
     playerScore++;
-    console.log(`You won using ${playerChoice} against ${computerSelection}`);
+    resultBox.textContent = `You won using ${playerChoice} against ${computerSelection}`;
   } else if (playerChoice == "rock" && computerSelection == "paper") {
     computerScore++;
-    console.log(`You lost using ${playerChoice} against ${computerSelection}`);
+    resultBox.textContent = `You lost using ${playerChoice} against ${computerSelection}`;
   } else if (playerChoice == "paper" && computerSelection == "rock") {
     playerScore++;
-    console.log(`You won using ${playerChoice} against ${computerSelection}`);
+    resultBox.textContent = `You won using ${playerChoice} against ${computerSelection}`;
   } else if (playerChoice == "paper" && computerSelection == "scissors") {
     computerScore++;
-    console.log(`You lost using ${playerChoice} against ${computerSelection}`);
+    resultBox.textContent = `You lost using ${playerChoice} against ${computerSelection}`;
   } else if (playerChoice == "scissors" && computerSelection == "paper") {
     playerScore++;
-    console.log(`You won using ${playerChoice} against ${computerSelection}`);
+    resultBox.textContent = `You won using ${playerChoice} against ${computerSelection}`;
   } else if (playerChoice == "scissors" && computerSelection == "rock") {
     computerScore++;
-    console.log(`You lost using ${playerChoice} against ${computerSelection}`);
-  } else {
-    console.log(`Please Enter Rock, Paper Or Scissors`);
+    resultBox.textContent = `You lost using ${playerChoice} against ${computerSelection}`;
   }
 }
