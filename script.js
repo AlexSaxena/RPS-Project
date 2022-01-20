2c64883254a585ac2ec22f92001b1b309b6a3e75
AlexSaxena <saxenaalex@gmail.com>
2022-01-20 19:53:05 +0100

    Adds Event listeners to buttons + console result

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ console.log("Greetings, General Kenobi");
 let playerScore = 0;
 let computerScore = 0;
 
+let rockBtn = document.querySelector("#rock-button");
+let paperBtn = document.querySelector("#paper-button");
+let scissorsBtn = document.querySelector("#scissors-button");
+
+rockBtn.addEventListener("click", playRound);
+paperBtn.addEventListener("click", playRound);
+scissorsBtn.addEventListener("click", playRound);
+
 // Function for Computer Choice
 function computerChoice() {
   let random = Math.floor(Math.random() * 3) + 1;
@@ -15,31 +23,34 @@ function computerChoice() {
 }
 
 // Function for playing a single round of RPS
-function playRound(playerSelection, computerSelection = computerChoice()) {
-  let playerChoice = playerSelection.toLowerCase().trim();
+function playRound(player) {
+  let computerSelection = computerChoice();
+  let playerChoice = player.target.innerText.toLowerCase();
   console.log("-------------------------------------------------");
 
   if (playerChoice == computerSelection) {
-    return `It is a tie between! ${playerChoice} and ${computerSelection}`;
+    console.log(
+      `It is a tie between! ${playerChoice} and ${computerSelection}`
+    );
   } else if (playerChoice == "rock" && computerSelection == "scissors") {
     playerScore++;
-    return `You won using ${playerChoice} against ${computerSelection}`;
+    console.log(`You won using ${playerChoice} against ${computerSelection}`);
   } else if (playerChoice == "rock" && computerSelection == "paper") {
     computerScore++;
-    return `You lost using ${playerChoice} against ${computerSelection}`;
+    console.log(`You lost using ${playerChoice} against ${computerSelection}`);
   } else if (playerChoice == "paper" && computerSelection == "rock") {
     playerScore++;
-    return `You won using ${playerChoice} against ${computerSelection}`;
+    console.log(`You won using ${playerChoice} against ${computerSelection}`);
   } else if (playerChoice == "paper" && computerSelection == "scissors") {
     computerScore++;
-    return `You lost using ${playerChoice} against ${computerSelection}`;
+    console.log(`You lost using ${playerChoice} against ${computerSelection}`);
   } else if (playerChoice == "scissors" && computerSelection == "paper") {
     playerScore++;
-    return `You won using ${playerChoice} against ${computerSelection}`;
+    console.log(`You won using ${playerChoice} against ${computerSelection}`);
   } else if (playerChoice == "scissors" && computerSelection == "rock") {
     computerScore++;
-    return `You lost using ${playerChoice} against ${computerSelection}`;
+    console.log(`You lost using ${playerChoice} against ${computerSelection}`);
   } else {
-    return `Please Enter Rock, Paper Or Scissors`;
+    console.log(`Please Enter Rock, Paper Or Scissors`);
   }
 }
